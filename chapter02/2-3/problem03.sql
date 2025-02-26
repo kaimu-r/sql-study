18f68727e79ed6ac1e4b643e0b3272282af6925c
kaimu-r <kaimu.seino@rizapgroup.com>
2025-02-26 11:46:23 +0900

    chapter2の練習問題完了

---
+++ chapter02/2-3/problem03.sql
@@ -0,0 +1,11 @@
+-- 問1
+select コード, 地域, 都道府県名, 県庁所在地, 面積
+from 都道府県
+
+-- 問2
+select *
+from 都道府県
+
+-- 問3
+select 地域 as area, 都道府県名 as pref
+from 都道府県
---
+++ chapter02/2-6/problem06.sql
@@ -0,0 +1,3 @@
+-- 問1
+delete from 都道府県
+where コード = 40
