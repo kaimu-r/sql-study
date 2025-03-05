f52a52584adf6cff6d7f14804e73d7be7121149e
kaimu-r <kaimu.seino@rizapgroup.com>
2025-03-05 16:03:13 +0900

    chapter4の練習問題完了

---
+++ chapter04/problem01.sql
@@ -0,0 +1,31 @@
+-- 問1
+SELECT * FROM 注文履歴
+ORDER BY 注文番号, 注文枝番
+
+-- 問2
+SELECT * FROM 注文履歴
+WHERE 日付 >= '2024-01-01' AND 日付 <= '2024-01-31'
+ORDER BY 商品名
+
+-- 問3
+SELECT 注文番号, 注文枝番, 注文金額 FROM 注文履歴
+WHERE 分類 = '1'
+ORDER BY 注文金額
+OFFSET 1 ROWS
+FETCH NEXT 3 ROWS ONLY
+
+-- 問4
+SELECT 日付, 商品名, 単価, 数量, 注文金額 FROM 注文履歴
+WHERE 分類 = '3' AND 数量 >= 2
+ORDER BY 日付, 数量 DESC
+
+-- 問5
+SELECT 分類, 商品名, サイズ, 単価 FROM 注文履歴
+WHERE 分類 = '1'
+UNION
+SELECT 分類, 商品名, NULL, 単価 FROM 注文履歴
+WHERE 分類 = '2'
+UNION
+SELECT 分類, 商品名, NULL, 単価 FROM 注文履歴
+WHERE 分類 = '3'
+ORDER BY 分類, 商品名
